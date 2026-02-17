26132f9fb279814538dc07510bfe4762746274dc
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2026-02-17 15:10:01 +0100

    express. server

---
+++ index.js
@@ -0,0 +1,7 @@
+import { toUpperCase } from "./utils.js";
+
+import name from "./utils.js";
+
+console.log(toUpperCase("moslem"));
+
+console.log(name);
---
+++ utils.js
@@ -0,0 +1,12 @@
+export function toUpperCase(str) {
+  return str.toUpperCase();
+}
+
+
+export const calculate = (num1, num2) => {
+  return num1 + num2;
+}
+
+const name = "Moslem Ajra";
+
+export default name;
