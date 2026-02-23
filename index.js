70f1b8b2332d9f383e2f06a675aebae52a5406d0
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2026-02-23 14:28:32 +0100

    update course

--- index.js
+++ index.js
@@ -1,7 +1,82 @@
-import { toUpperCase } from "./utils.js";
+import express from "express";
 
-import name from "./utils.js";
+let courses = [
+  {
+    id: 1,
+    title: "Introduction to Web Development",
+    price: 500,
+    description: "This course will teach you the basics of web development",
+    instructor: "John Doe",
+  },
+  {
+    id: 2,
+    title: "Advanced Web Development",
+    price: 1000,
+    description:
+      "This course will teach you advanced web development techniques",
+    instructor: "Jane Doe",
+  },
+  {
+    id: 3,
+    title: "Python Programming",
+    price: 800,
+    description: "This course will teach you the basics of Python programming",
+    instructor: "Bob Smith",
+  },
+];
 
-console.log(toUpperCase("moslem"));
+const app = express();
+app.use(express.json());
+// http://localhost:3000
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To our Server....</h1>");
+});
 
-console.log(name);
+app.get("/api/courses", (req, res) => {
+  res.status(200).send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const { id } = req.params;
+
+  const course = courses.find((course) => course.id === +id);
+
+  if (!course) {
+    return res.status(404).send("Course not found");
+  }
+
+  res.status(200).send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const newCourse = { id: courses.length + 1, ...req.body };
+  courses.push(newCourse);
+  res.status(201).send(newCourse);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const { id } = req.params;
+
+  const course = courses.find((course) => course.id === +id);
+
+  if (!course) {
+    return res.status(404).send("Course not found");
+  }
+
+  courses = courses.map((course) =>
+    course.id === +id ? { ...course, ...req.body } : course,
+  );
+
+  res.status(200).send(courses);
+});
+
+
+app.delete("/api/courses/:id",  (req , res) => {
+
+
+
+})
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
